87ff38cdc52e41b18049743346eeaf374049331a
Wechuli <wechulipaul@yahoo.com>
2019-10-03 08:55:02 +0300

    adding a gamescreen and random guess logic

--- guess_game/App.js
+++ guess_game/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View, AppRegistry } from "react-native";
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import { name as appName } from "./app.json";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
       <StartGameScreen />
+      <GameScreen/>
     </View>
   );
 }
---
+++ guess_game/screens/GameScreen.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  if (randomNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  }
+  return randomNumber;
+};
+
+const GameScreen = props => {
+  const { userChoice } = props;
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, userChoice)
+  );
+  return (
+    <View>
+      <Text>Opponent's guess</Text>
+      <NumberContainer selectedNumber={currentGuess} />
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" />
+        <Button title="GREATER" />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
+});
+export default GameScreen;
